0b434033c6ae88d3521655694549e29b4b8d2133
Melle Sieswerda <melle@sieswerda.net>
2017-05-24 16:31:07 +0200

    Added failsafe to the shell scripts to create the “logs” directory if it does not yet exist.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # find -E ./ -regex ".*\.(py|tpl)$" | entr sh -c "clear && ./generate.py"
+
+# Make sure the logs directory exists.
+mkdir -p logs
+
+# (re)generate the fhir module everytime one of the files in files.txt changes.  
 cat files.txt | entr sh -c "clear && ./generate.py"
